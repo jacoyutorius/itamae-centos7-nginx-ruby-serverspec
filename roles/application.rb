dda44be141775d38b1d4bc10793156024d4415a7
yuto <jacoyutorius@gmail.com>
2016-03-20 02:10:54 +0900

    add default role

--- roles/application.rb
+++ roles/application.rb
@@ -1,5 +1,19 @@
-# install nginx
-include_recipe "../cookbooks/nginx/default"
+include_recipe "../cookbooks/default"
 
-# install ruby
-include_recipe "../cookbooks/ruby/default"
+# # install nginx
+# include_recipe "../cookbooks/nginx/default"
+
+# # install ruby
+# include_recipe "../cookbooks/ruby/default"
+
+# # install elasticsearch
+# include_recipe "../cookbooks/elasticsearch"
+
+# # install kibana
+# include_recipe "../cookbooks/kibana"
+
+# # install td-agent
+# include_recipe "../cookbooks/td-agent/default"
+
+# # install grafana
+# include_recipe "../cookbooks/grafana/default"
