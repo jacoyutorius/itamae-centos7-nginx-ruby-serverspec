2ef49f313be3f0ebf235138eb88e7cf535e29d57
Yuto Ogi <jacoyutorius@gmail.com>
2016-05-19 00:14:42 +0900

    update Kibana version

--- cookbooks/kibana/default.rb
+++ cookbooks/kibana/default.rb
@@ -1,9 +1,11 @@
+kibana_url = "https://download.elastic.co/kibana/kibana"
+kibana_file = "kibana-4.5.0-linux-x64"
 
 commands = [
-  "wget https://download.elastic.co/kibana/kibana/kibana-4.4.2-linux-x64.tar.gz",
-  "tar -xzvf kibana-4.4.2-linux-x64.tar.gz",
-  "mv kibana-4.4.2-linux-x64 kibana",
-  "rm -f kibana-4.4.2-linux-x64.tar.gz",
+  "wget #{kibana_url}/#{kibana_file}.tar.gz",
+  "tar -xzvf #{kibana_file}.tar.gz ",
+  "mv #{kibana_file} kibana",
+  "rm -f #{kibana_file}.tar.gz",
   "chown vagrant:vagrant -R /var/www/html/kibana"
 ].each do |command|
   execute "#{command}" do
@@ -11,6 +13,14 @@ commands = [
   	cwd "/var/www/html"
     command command
   end
+end
+
+# fix "Kibana Unable to connect to elasticsearch at http://localhost:9200" error
+# https://discuss.elastic.co/t/kibana-unable-to-connect-to-elasticsearch-at-http-localhost-9200/29865
+template "/var/www/html/kibana/config/kibana.yml" do
+  user "root"
+  group "root"
+  source "./templates/var/www/html/kibana/config/kibana.yml"
 end
 
 execute "launch kibana" do
