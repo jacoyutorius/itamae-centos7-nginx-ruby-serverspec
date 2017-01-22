117b4a7b66290ad3fb2db8a3f609edf2f5deb6ea
Yuto Ogi <jacoyutorius@gmail.com>
2017-01-22 21:32:42 +0900

    fix kibana recipe

--- cookbooks/kibana/default.rb
+++ cookbooks/kibana/default.rb
@@ -1,30 +1,55 @@
-kibana_url = "https://download.elastic.co/kibana/kibana"
-kibana_file = "kibana-4.5.0-linux-x64"
+#
+#  cookbook - kibana
+#   install & defaylt setting for Kibana
+# 
+#   2016.12.24
+#
 
-commands = [
-  "wget #{kibana_url}/#{kibana_file}.tar.gz",
-  "tar -xzvf #{kibana_file}.tar.gz ",
-  "mv #{kibana_file} kibana",
-  "rm -f #{kibana_file}.tar.gz",
-  "chown vagrant:vagrant -R /var/www/html/kibana"
-].each do |command|
-  execute "#{command}" do
-    user "root"
-  	cwd "/var/www/html"
-    command command
-  end
+# execute "download kibana-5.1.1-linux-x86_64" do
+#   user "root"
+#   cwd "/root"
+#   command "wget https://artifacts.elastic.co/downloads/kibana/kibana-5.1.1-linux-x86_64.tar.gz"
+#   not_if "test -e /opt/kibana"
+# end
+
+# execute "tar kibana-5.1.1-linux-x86_64" do
+#   user "root"
+#   cwd "/root"
+#   command "tar -xzvf kibana-5.1.1-linux-x86_64.tar.gz"
+#   not_if "test -e /opt/kibana"
+# end
+
+# execute "tar kibana-5.1.1-linux-x86_64" do
+#   user "root"
+#   cwd "/root"
+#   command "mv kibana-5.1.1-linux-x86_64 /opt/kibana"
+#   not_if "test -e /opt/kibana"
+# end
+
+package "https://artifacts.elastic.co/downloads/kibana/kibana-5.1.2-x86_64.rpm" do
+  action :install
+  not_if "test -e /etc/kibana"
 end
 
-# fix "Kibana Unable to connect to elasticsearch at http://localhost:9200" error
-# https://discuss.elastic.co/t/kibana-unable-to-connect-to-elasticsearch-at-http-localhost-9200/29865
-template "/var/www/html/kibana/config/kibana.yml" do
+execute "backup config" do
+  user "root"
+  cwd "/etc/kibana"
+  command "cp kibana.yml kibana.yml_bk"
+end
+
+template "/etc/kibana/kibana.yml" do
   user "root"
   group "root"
-  source "./templates/var/www/html/kibana/config/kibana.yml"
+  mode "775"
+  source "./templates/etc/kibana/kibana.yml.erb"
 end
 
-execute "launch kibana" do
-  user "root"
-  cwd "/var/www/htm/kibana"
-  command "./bin/kibana > ./kibana.log &"
+directory "/opt/kibana/logs" do
+  mode "775"
+  owner "root"
+  group "root"
+end
+
+service "kibana" do
+  action [:start, :restart]
 end
