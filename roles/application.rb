0bf54b653b858b35c5af4710c24ca3d20f3fc8a9
Yuto Ogi <jacoyutorius@gmail.com>
2017-01-23 01:30:49 +0900

    change recipe executing order

--- roles/application.rb
+++ roles/application.rb
@@ -1,19 +1,17 @@
 include_recipe "../cookbooks/default"
 
-# # install nginx
-# include_recipe "../cookbooks/nginx/default"
+# install nginx
+include_recipe "../cookbooks/nginx/default"
 
-# # install ruby
-# include_recipe "../cookbooks/ruby/default"
+# install ruby
+include_recipe "../cookbooks/ruby/default"
 
-# # install elasticsearch
-# include_recipe "../cookbooks/elasticsearch"
+# install elasticsearch
+include_recipe "../cookbooks/elasticsearch"
 
-# # install kibana
-# include_recipe "../cookbooks/kibana"
+# install td-agent
+include_recipe "../cookbooks/td-agent/default"
 
-# # install td-agent
-# include_recipe "../cookbooks/td-agent/default"
+# install kibana
+include_recipe "../cookbooks/kibana"
 
-# # install grafana
-# include_recipe "../cookbooks/grafana/default"
