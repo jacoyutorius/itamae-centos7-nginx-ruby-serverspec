3b41dfd338538cb0d11e2284f3de5b453250e9e5
yutoogi <yuto.ogi@fourier.jp>
2017-02-25 15:15:19 +0900

    railsもインストールするようレシピを修正
    Please enter the commit message for your changes. Lines starting

--- cookbooks/ruby/default.rb
+++ cookbooks/ruby/default.rb
@@ -1,8 +1,11 @@
+rbenv_path = "/usr/local/rbenv"
+gem_path = "#{rbenv_path}/shims/gem" 
+
 # use itamae-plugin-rbenv gem
 include_recipe "rbenv::system"
 
 [
-  'export RBENV_ROOT=/usr/local/rbenv',
+  "export RBENV_ROOT=#{rbenv_path}",
   'export PATH="${RBENV_ROOT}/bin:${PATH}"',
   'eval "$(rbenv init -)"'
 ].each do |command|
@@ -10,4 +13,9 @@ include_recipe "rbenv::system"
     user "root"
     command "echo '#{command}' >> ~/.bashrc"
   end
+end
+
+execute "install rails" do
+	user "root"
+	command "#{gem_path} install rails --no-rdoc --no-ri"
 end
